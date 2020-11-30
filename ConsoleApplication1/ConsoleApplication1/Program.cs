a7182d4384c67736fc82c9e7fd34e7ecca2f4b92
Billy Jefferson <biljef232@gmail.com>
2020-11-30 11:49:59 +0700

    Penggantian nama dan penambahan keterangan NIM

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Hello, Nama Saya Billy Jefferson ");
+            Console.WriteLine("NIM saya adalah : 0706022010003 ");
         }
     }
 }
