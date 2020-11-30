a385a94f83134b4a51cbedf7fa01a93e26695cd8
michelle chandra <chandramichelle40@gmail.com>
2020-11-30 11:49:41 +0700

    perubahan nama dan penambahan detail NIM

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Nama saya Michelle");
+            Console.WriteLine("NIM saya adalah : 0706022010019");
             Console.ReadKey();
         }
     }
