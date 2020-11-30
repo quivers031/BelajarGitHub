dcea45521d0e44e693c737e6d1cd3b4031937bf2
BillyAnthonyB <BillyAnthonyB@github.com>
2020-11-30 11:50:12 +0700

    Penggantian nama dan penambahan keterangan NIM

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, nama saya Billy Anthony");
+            Console.WriteLine("NIM saya adalah : 0706022010014");
             Console.ReadKey();
         }
     }
