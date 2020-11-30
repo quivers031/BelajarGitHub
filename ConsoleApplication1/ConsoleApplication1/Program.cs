2e0e0afc4b56bebf7a1b72c407088d92346f5346
HiItsNiko <erickgautama54@yahoo.co.id>
2020-11-30 12:49:28 +0800

    Nama, Nim and the TRUTH!!!

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Hello, nama saya Erick");
+            Console.WriteLine("Nim saya adalah: 0706022010029");
+            Console.WriteLine("want to know about the truth?");
+            string choice = Console.ReadLine();
+            if (choice == "yes")
+            {
+                Console.WriteLine("According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don't care what humans think is impossible.");
+                Console.WriteLine();
+                Console.WriteLine("https://www.youtube.com/watch?v=-y3tc2tyb3c");
+                Console.WriteLine();
+                Console.WriteLine("The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding... Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark. Get ready!… Start.");
+            }
+            
         }
     }
 }
+    
